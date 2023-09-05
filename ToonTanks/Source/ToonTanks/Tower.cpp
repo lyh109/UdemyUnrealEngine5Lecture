b249e5951407e0aca99b31b4c6b2a8ce140f39ad
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2023-09-06 00:23:34 +0900

    The Tower Class

---
+++ ToonTanks/Source/ToonTanks/Tower.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
+
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (Tank)
+	{
+		float distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+		if (distance <= FireRange)
+		{
+			RotateTurret(Tank->GetActorLocation());
+		}
+	}
+}
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
