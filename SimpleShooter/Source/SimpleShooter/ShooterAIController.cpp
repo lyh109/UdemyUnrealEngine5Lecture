f440aa6d2ac9d94211df51a83e56b2a98a9f1827
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2023-11-12 20:31:34 +0900

    AI Aiming

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -2,4 +2,12 @@
 
 
 #include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
 
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	SetFocus(playerPawn);
+}
