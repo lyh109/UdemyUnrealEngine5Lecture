6e991eed422009baa59df7a30c555f7a3c26d958
DESKTOP-UOQM1PH\YANG <meekk1009@gmail.com>
2023-12-05 23:52:42 +0900

    BT Decorators And Selectors

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ShooterAIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -23,16 +22,15 @@ void AShooterAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	
-	/*APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-	 if (LineOfSightTo(playerPawn))
-	 {
-	 	SetFocus(playerPawn);
-	 	MoveToActor(playerPawn, AcceptanceRadius);
-	 }
-	 else
-	 {
-	 	ClearFocus(EAIFocusPriority::Gameplay);
-	 	StopMovement();
-	 }*/
+	if (LineOfSightTo(playerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), playerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), playerPawn->GetActorLocation());
+	}
+	else
+	{
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	}
 }
